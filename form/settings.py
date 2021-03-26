07940300b33326d274c91147e06490ac9a0c2efd
prosy <pdarceno@gmail.com>
2021-03-26 17:46:38 +0800

    remove key

--- form/settings.py
+++ form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3+cel^y-*)c9_yz+jkr3vd7vx238vwthd-(hdyo1(xcqg+=sgn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
