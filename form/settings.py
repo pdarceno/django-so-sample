58d60f2664a64daa17f89fd68451d33014f38426
prosy <pdarceno@gmail.com>
2021-03-28 08:25:30 +0800

    add cors

--- form/settings.py
+++ form/settings.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
 	'polls.apps.PollsConfig',
 	'api.apps.ApiConfig',
 	'rest_framework',
-
+	'corsheaders',
+	
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
+
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,7 @@ MIDDLEWARE = [
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'form.urls'
 
 TEMPLATES = [
